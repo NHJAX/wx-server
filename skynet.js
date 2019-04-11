98362fca162fb91327e6c02eea371f8b915c1001
Alex Rangeo <arangeo@conceptplusllc.com>
2019-04-11 14:58:57 -0400

    update data passed

--- skynet.js
+++ skynet.js
@@ -13,8 +13,8 @@ const WAIT_TIME_PARAM = "wait/times"
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // trust self signed certificate
 
 var client = mqtt.connect('mqtt://localhost'); //IP of the machine which the server is hosted
-var SECURE_KEY = path.join(WORKING_DIR, 'certs' + '/wxKey.pem');//Location of secure key - path to key only, DO NOT READ THE KEY
-var SECURE_CERT = path.join(WORKING_DIR, 'certs' + '/wxCert.pem');//Location of Secure Cert - path to key only, DO NOT READ THE CERT
+var SECURE_KEY = path.join(WORKING_DIR, 'certs' + '/wxKey.pem'); //Location of secure key - path to key only, DO NOT READ THE KEY
+var SECURE_CERT = path.join(WORKING_DIR, 'certs' + '/wxCert.pem'); //Location of Secure Cert - path to key only, DO NOT READ THE CERT
 
 var settings = { //Server settings
     //port: 1883,
@@ -29,54 +29,58 @@ var settings = { //Server settings
 //Server Setup
 var server = new mosca.Server(settings);
 
-var readyFunc = function(){
+var readyFunc = function() {
     console.log('inside ready');
     server.on('clientConnected', clientConnectedFunc);
     server.on('published', publishedFunc);
 
 };
-var clientConnectedFunc = function(client){
+var clientConnectedFunc = function(client) {
     console.log('inside client connected');
 };
-var publishedFunc = function(packet, client){
+var publishedFunc = function(packet, client) {
     console.log('inside published');
 
     if (client) {
-            var buf = (Buffer.from(packet.payload)); //Buffer is dumped from packet
-            let msg = (buf.toString()); //Buffer is converted to string
-            msg = JSON.parse(msg);
-            console.log("Message from MQTT ", msg);
-            //var msg = {"location":"jax","temp":"25.0","hum":"39.0"}
-            // var msg = { "temperature": 28,"humidity": 69, "location": 'jax'};
-            var headers = {
-                'NHJax-API-Key': API_CONFIG["NHJax-API-Key"]
-            };
-            console.log(msg.location);
+        let buf = (Buffer.from(packet.payload)); //Buffer is dumped from packet
 
-            console.log(msg);
-            // console.log(JSON.stringify(msg));
-            // console.log(JSON.parse(msg));
+        let msg = (buf.toString()); //Buffer is converted to string
+        // msg = JSON.parse(msg);
 
-            axios({
-                    method: "post",
-                    url: API_CONFIG.BASEURL + msg.MQTT_TOPIC,
-                    headers,
-                    data: {
-                        "location": msg.location,
-                        "temperature": Number(msg.temp),
-                        "humidity": Number(msg.hum)
-                    }
-                })
-                .then(res => {
-                    console.log('res');
-                })
-                .catch(err => {
-                    console.log('err');
-                })
-                client.close();
+        console.log("Message from MQTT ", msg);
+        let headers = {
+            'NHJax-API-Key': API_CONFIG["NHJax-API-Key"]
         };
+        console.log(msg.location);
+
+        //console.log(msg);
+        console.log(JSON.stringify(msg));
+        console.log(JSON.parse(msg));
+
+        let data = {
+            "location": msg.location,
+            "temperature": Number(msg.temp),
+            "humidity": Number(msg.hum)
+        }
+
+        console.log('data', data);
+
+        axios({
+                method: "post",
+                url: API_CONFIG.BASEURL + msg.MQTT_TOPIC,
+                headers,
+                data
+            })
+            .then(res => {
+                console.log('res');
+            })
+            .catch(err => {
+                console.log('err');
+            })
+        client.close();
+    };
 
 
-    
+
 };
 server.on('ready', readyFunc);
