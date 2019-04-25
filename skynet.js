0c21db9911aaa6b874b55c2fa7e90eb974c65711
Kevin nichols <kevinnichols1321@icloud.com>
2019-04-25 06:53:39 -0400

    open port 1883

--- skynet.js
+++ skynet.js
@@ -17,12 +17,12 @@ var SECURE_KEY = path.join(WORKING_DIR, 'certs' + '/wxKey.pem'); //Location of s
 var SECURE_CERT = path.join(WORKING_DIR, 'certs' + '/wxCert.pem'); //Location of Secure Cert - path to key only, DO NOT READ THE CERT
 
 var settings = { //Server settings
-    //port: 1883,
+    port: 1883,
     secure: {
         port: 8883, //Secure MQTT port
         keyPath: SECURE_KEY,
         certPath: SECURE_CERT,
-        allowNonSecure: false //Denies Nonsecure Connections 
+        allowNonSecure: false //Denies Nonsecure Connections
     }
     //
 };
