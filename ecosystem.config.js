3868582972cba39defd716e9a26ebdd016e7eefb
Kevin nichols <kevinnichols1321@icloud.com>
2019-05-29 07:12:30 -0400

    added lightning app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,6 +28,21 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
+  },
+  {
+    name: 'LightningApp',
+    script: 'node /lightningapp/Lightning.js',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }
   ]
 };
