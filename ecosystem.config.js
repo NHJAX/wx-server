0bd0a0c9b28dfa917958c3c258a8c1e8b5400385
Kevin nichols <kevinnichols1321@icloud.com>
2019-05-29 07:54:03 -0400

    move the lightning file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   {
     name: 'LightningApp',
-    script: 'node ./lightningApp/lightning.js',
+    script: 'node lightning.js',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
