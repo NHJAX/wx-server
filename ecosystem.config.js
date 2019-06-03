3aa517010879f086c04fb16c966277e8d57d2eea
Kevin nichols <kevinnichols1321@icloud.com>
2019-06-03 08:56:31 -0400

    turned on lighting app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,21 +29,21 @@ module.exports = {
       NODE_ENV: 'production'
     }
   }
-  // ,
-  // {
-  //   name: 'LightningApp',
-  //   script: 'node lightning.js',
-  //   // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-  //   instances: "max",
-  //   autorestart: true,
-  //   watch: false,
-  //   max_memory_restart: '1G',
-  //   env: {
-  //     NODE_ENV: 'development'
-  //   },
-  //   env_production: {
-  //     NODE_ENV: 'production'
-  //   }
-  // }
+  ,
+  {
+    name: 'LightningApp',
+    script: 'node lightning.js',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: "max",
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }
   ]
 };
